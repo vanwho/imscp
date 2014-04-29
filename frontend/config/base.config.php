e6348a32667ca976ee1cc2045ff156b04f74d3ae
Laurent Declercq <l.declercq@nuxwin.com>
2014-04-29 06:59:14 +0200

    Small fixes

--- frontend/config/base.config.php
+++ frontend/config/base.config.php
@@ -6,6 +6,7 @@ return array(
 		'ZendDeveloperTools',
 		'DoctrineModule',
 		'DoctrineORMModule',
+		'ImscpBase',
 	),
 
 	// These are various options for the listeners attached to the ModuleManager
--- frontend/public/index.php
+++ frontend/public/index.php
@@ -13,5 +13,5 @@ if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['RE
 // Setup autoloading
 require 'init_autoloader.php';
 
-// Run the application!
-Zend\Mvc\Application::init(require 'config/application.config.php')->run();
+// Run the frontend
+Zend\Mvc\Application::init(require 'config/base.config.php')->run();
