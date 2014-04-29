ffd643c3c1bd9451297e7c580765f44a80af67fb
Laurent Declercq <l.declercq@nuxwin.com>
2014-04-29 08:13:57 +0200

    Added bootstrap files + small fixes

--- frontend/config/base.config.php
+++ frontend/config/base.config.php
@@ -30,26 +30,26 @@ return array(
 		// Whether or not to enable a configuration cache.
 		// If enabled, the merged configuration will be cached and used in
 		// subsequent requests.
-		'config_cache_enabled' => false,
+		//'config_cache_enabled' => false,
 
 		// The key used to create the configuration cache file name.
-		'config_cache_key' => 'config.cache',
+		//'config_cache_key' => 'config.cache',
 
 		// Whether or not to enable a module class map cache.
 		// If enabled, creates a module class map cache which will be used
 		// by in future requests, to reduce the autoloading process.
-		'module_map_cache_enabled' => false,
+		//'module_map_cache_enabled' => false,
 
 		// The key used to create the class map cache file name.
-		'module_map_cache_key' => 'module.classmap.cache',
+		//'module_map_cache_key' => 'module.classmap.cache',
 
 		// The path in which to cache merged configuration.
-		'cache_dir' => dirname(__DIR__) . '/data/cache',
+		//'cache_dir' => dirname(__DIR__) . '/data/cache',
 
 		// Whether or not to enable modules dependency checking.
 		// Enabled by default, prevents usage of modules that depend on other modules
 		// that weren't loaded.
-		'check_dependencies' => true,
+		//'check_dependencies' => false,
 	),
 
 	// Used to create an own service manager. May contain one or more child arrays.
