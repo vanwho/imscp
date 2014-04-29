42234b6f4521c4129e8c0ef7d2f3e8d88ec37627
Laurent Declercq <l.declercq@nuxwin.com>
2014-04-29 06:17:42 +0200

    Small fixes

---
+++ frontend/module/Base/config/module.config.php
@@ -0,0 +1,31 @@
+<?php
+
+return array(
+	/*
+	'translator' => array(
+		'locale' => 'en_US',
+		'translation_file_patterns' => array(
+			array(
+				'type'     => 'gettext',
+				'base_dir' => __DIR__ . '/../language',
+				'pattern'  => '%s.mo',
+			),
+		),
+	),
+	*/
+	'view_manager' => array(
+		'display_not_found_reason' => true,
+		'display_exceptions' => true,
+		'doctype' => 'HTML5',
+		'not_found_template' => 'error/404',
+		'exception_template' => 'error/exception',
+		'template_map' => array(
+			'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+			'error/404' => __DIR__ . '/../view/error/404.phtml',
+			'error/exception' => __DIR__ . '/../view/error/exception.phtml',
+		),
+		'template_path_stack' => array(
+			__DIR__ . '/../view',
+		),
+	),
+);
---
+++ frontend/module/Base/src/Module.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ImscpBase;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+/**
+ * Class Module
+ *
+ * @package ImscpBase
+ */
+class Module implements ConfigProviderInterface, AutoloaderProviderInterface
+{
+	/**
+	 * Returns configuration to merge with application configuration
+	 *
+	 * @return array|\Traversable
+	 */
+	public function getConfig()
+	{
+		return include __DIR__ . '/config/module.config.php';
+	}
+
+	/**
+	 * Return an array for passing to Zend\Loader\AutoloaderFactory
+	 *
+	 * @return array
+	 */
+	public function getAutoloaderConfig()
+	{
+		return array(
+			'Zend\Loader\StandardAutoloader' => array(
+				'namespaces' => array(
+					__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+				),
+			),
+		);
+	}
+}
