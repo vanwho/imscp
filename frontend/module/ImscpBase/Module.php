1e9e5dcc8313b4ed11564358f44ae2dc7578d705
Laurent Declercq <l.declercq@nuxwin.com>
2014-04-29 12:38:43 +0200

    CS fixes

--- frontend/module/ImscpBase/Module.php
+++ frontend/module/ImscpBase/Module.php
@@ -12,29 +12,29 @@ use Zend\ModuleManager\Feature\ConfigProviderInterface;
  */
 class Module implements ConfigProviderInterface, AutoloaderProviderInterface
 {
-	/**
-	 * Returns configuration to merge with base configuration
-	 *
-	 * @return array
-	 */
-	public function getConfig()
-	{
-		return include __DIR__ . '/config/module.config.php';
-	}
+    /**
+     * Returns configuration to merge with base configuration
+     *
+     * @return array
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
 
-	/**
-	 * Return an array for passing to Zend\Loader\AutoloaderFactory
-	 *
-	 * @return array
-	 */
-	public function getAutoloaderConfig()
-	{
-		return array(
-			'Zend\Loader\StandardAutoloader' => array(
-				'namespaces' => array(
-					__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-				),
-			),
-		);
-	}
+    /**
+     * Return an array for passing to Zend\Loader\AutoloaderFactory
+     *
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
 }
--- frontend/module/ImscpBase/config/module.config.php
+++ frontend/module/ImscpBase/config/module.config.php
@@ -1,47 +1,47 @@
 <?php
 
 return array(
-	/*
-	'translator' => array(
-		'locale' => 'en_US',
-		'translation_file_patterns' => array(
-			array(
-				'type'     => 'gettext',
-				'base_dir' => __DIR__ . '/../language',
-				'pattern'  => '%s.mo',
-			),
-		),
-	),
-	*/
-	'view_manager' => array(
-		'display_not_found_reason' => true,
-		'display_exceptions' => true,
-		'doctype' => 'HTML5',
-		'not_found_template' => 'error/404',
-		'exception_template' => 'error/exception',
-		'template_map' => array(
-			'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
-			'error/404' => __DIR__ . '/../view/error/404.phtml',
-			'error/exception' => __DIR__ . '/../view/error/exception.phtml',
-		),
-		'template_path_stack' => array(
-			__DIR__ . '/../view',
-		),
-	),
+    /*
+    'translator' => array(
+        'locale' => 'en_US',
+        'translation_file_patterns' => array(
+            array(
+                'type'     => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern'  => '%s.mo',
+            ),
+        ),
+    ),
+    */
+    'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions' => true,
+        'doctype' => 'HTML5',
+        'not_found_template' => 'error/404',
+        'exception_template' => 'error/exception',
+        'template_map' => array(
+            'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+            'error/404' => __DIR__ . '/../view/error/404.phtml',
+            'error/exception' => __DIR__ . '/../view/error/exception.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
 
-	'asset_manager' => array(
-		'resolver_configs' => array(
-			'paths' => array(
-				__DIR__ . '/../assets',
-			)
-		),
-		'caching' => array(
-			'default' => array(
-				'cache' => 'FilePath',
-				'options' => array(
-					'dir' => 'public'
-				),
-			)
-		),
-	),
+    'asset_manager' => array(
+        'resolver_configs' => array(
+            'paths' => array(
+                __DIR__ . '/../assets',
+            )
+        ),
+        'caching' => array(
+            'default' => array(
+                'cache' => 'FilePath',
+                'options' => array(
+                    'dir' => 'public'
+                ),
+            )
+        ),
+    ),
 );
